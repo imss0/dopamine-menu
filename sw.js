18b0452a7c7317680de90a8e6a07109cfe57a4ec
Sohyun Lim <shlim0410@gmail.com>
2026-09-04 23:40:17 +0100

    Simplify service worker install and activate logic
    Refactor service worker to simplify install and activate events, removing cache management.

--- sw.js
+++ sw.js
@@ -1,34 +1,5 @@
-const CACHE_NAME = "dopamine-menu-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-180.png",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+  caches.keys().then(keys => keys.forEach(k => caches.delete(k)));
+  self.registration.unregister();
 });
